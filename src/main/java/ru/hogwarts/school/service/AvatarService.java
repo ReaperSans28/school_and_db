f5f56d036d3ea71e387dcccf14c3687d7ed5aff6
ReaperSans28 <smileuwu321@gmail.com>
2025-05-12 16:08:25 +0300

    Logging and configuration in the application

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -7,9 +7,13 @@ import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.repositories.AvatarRepository;
 
+import java.util.logging.Logger;
+
 @Service
 public class AvatarService {
     private final AvatarRepository avatarRepository;
+
+    Logger logger = Logger.getLogger(AvatarService.class.getName());
 
     @Autowired
     public AvatarService(AvatarRepository avatarRepository) {
@@ -17,7 +21,8 @@ public class AvatarService {
     }
 
     public Page<Avatar> getAllAvatars(Integer pageNumber, Integer pageSize) {
+        logger.info( "Was invoked method for get avatars" );
         PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
         return avatarRepository.findAll(pageRequest);
     }
-} 
+}
--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.service;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,27 +15,35 @@ public class FacultyService {
     @Autowired
     private FacultyRepository repository;
 
+    Logger logger = Logger.getLogger(FacultyService.class.getName());
+
     public Faculty addFaculty(Faculty faculty) {
+        logger.info("Creating faculty: " + faculty);
         return repository.save(faculty);
     }
 
     public Optional<Faculty> findFaculty(long id) {
+        logger.info("Finding faculty with id: " + id);
         return repository.findById(id);
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        logger.info("Updating faculty: " + faculty);
         return repository.save(faculty);
     }
 
     public void deleteFaculty(long id) {
+        logger.info("Deleting faculty with id: " + id);
         repository.deleteById(id);
     }
 
     public Optional<Faculty> findByStudentId(long id) {
+        logger.info("Finding student with id: " + id);
         return repository.findByStudents_Id(id);
     }
 
     public Optional<Faculty> searchByNameOrColor(String name, String color) {
+        logger.info("Finding faculties by color and name: " + name + " and color: " + color);
         return repository.searchByNameOrColor(name, color);
     }
 }
